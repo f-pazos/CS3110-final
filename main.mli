1992cbde599ffdda3d29ea285f67526be96ee2a9
Sourabh Chakraborty <sc2356@cornell.edu>
2018-03-29 16:00:18 -0400

    main mli file

---
+++ main.mli
@@ -0,0 +1,7 @@
+(* [play_game s] starts a game with the state s, and uses a REPL to
+ * prompt the user for the number of steps the game should take *)
+val play_game : state -> ()
+
+(* [main] starts a REPL that prompts the user for a state file or
+ * paramters for starting the game, and then starts the game *)
+val main : () -> ()
