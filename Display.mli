01de055b1ba3ea9b5bef5e90741bf31d2b936e2c
Felipe Alejandro Pazos <fap26@cornell.edu>
2018-03-29 20:36:31 -0400

    Update Display.mli
    Updated Display.mli

--- Display.mli
+++ Display.mli
@@ -1,14 +1,9 @@
-module Display = sig 
-	
-	(* An abstract type that maintains what the world map actually looks like. 
-	 *)
-	type world_map
+(* An abstract type that maintains what the world map actually looks like. 
+ *)
+type world_map
 
-
-	(* [display state] will create a graphic representation for the current 
-	   simulation state, so the user can visually see how the world is
-	   progressing. 
-	 *)
-	val display : state -> unit
-
-end
+(* [display state] will create a graphic representation for the current 
+   simulation state, so the user can visually see how the world is
+   progressing. 
+*)
+val display : state -> unit
