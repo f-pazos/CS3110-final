ec4e9f7032f6444aa9c5306710b0d1c2bc5ef4e5
Sourabh Chakraborty <sc2356@cornell.edu>
2018-05-12 16:26:08 -0400

    started main

---
+++ main.ml
@@ -0,0 +1,9 @@
+open State
+
+(* [play_game s] starts a game with the state s, and uses a REPL to
+ * prompt the user for the number of steps the game should take *)
+let rec play_game s =
+  failwith "Undefined"
+
+let rec main () =
+  failwith "Undefined"
